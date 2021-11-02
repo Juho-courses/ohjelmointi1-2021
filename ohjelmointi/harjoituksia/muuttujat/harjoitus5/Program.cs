493ff317798bada9462ced85d2020f095ca5e746
Juho Salli <juho.salli@gmail.com>
2021-11-02 12:53:08 +0200

    muuttuja harkat 5 ja 6

---
+++ ohjelmointi/harjoituksia/muuttujat/harjoitus5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace harjoitus5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string etunimi = "Juho"; 
+           string sukunimi = "Salli";
+
+           Console.WriteLine(etunimi + " " + sukunimi);
+           Console.WriteLine($"{etunimi} {sukunimi}");
+
+        }
+    }
+}
---
+++ ohjelmointi/harjoituksia/muuttujat/harjoitus6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace harjoitus6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int a,b,c;
+           a = 12; 
+           b = a * 4;
+           a = 25;
+           c = (b + a) / a;
+           Console.WriteLine(c);
+        }
+    }
+}
