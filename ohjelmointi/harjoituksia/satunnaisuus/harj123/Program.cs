4e902447c92c5ce1730431cdfc2ac7305c15d454
Juho Salli <juho.salli@gmail.com>
2021-12-14 12:31:06 +0200

    satunnaisuusharkat

---
+++ ohjelmointi/harjoituksia/satunnaisuus/harj123/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace harj123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Eka();
+            // Toka();
+            Kolmas();
+        }
+        
+        static void Eka() {
+            Random r = new Random();
+            Console.WriteLine(r.Next(0, 100));
+        }
+
+        static void Toka () {
+            Console.Write("luku 1: ");
+            string vastaus = Console.ReadLine();
+            int luku1 = int.Parse(vastaus);
+            Console.Write("luku 2: ");
+            vastaus = Console.ReadLine();
+            int luku2 = int.Parse(vastaus);
+
+            if (luku1 > 0 && luku2 > 0) {
+                Random r = new Random();
+                int rnd = r.Next(luku1, luku2);
+                Console.WriteLine(rnd);
+            }
+        }
+
+        static void Kolmas() {
+            Console.Write("luku 1: ");
+            string vastaus = Console.ReadLine();
+            int luku1 = int.Parse(vastaus);
+
+            Random r = new Random();
+            for (int i = 0; i < luku1; i++) {
+                Console.WriteLine(r.Next(1, 50));
+            }
+        }
+    }
+}
---
+++ ohjelmointi/harjoituksia/satunnaisuus/lotto/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+
+namespace lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.Write("montako riviä: ");
+            // string tmp = Console.ReadLine();
+            // int maara = int.Parse(tmp);
+
+            // for (int i = 0; i < maara; i++) {
+            //     int[] rivi = LuoRivi();
+            //     TulostaRivi(rivi);
+            // }
+
+            Console.Write("anna 7 numeroa, pilkuilla eroteltuna: ");
+            string[] luvut_str = Console.ReadLine().Split(',');
+
+            int[] rivi = new int[luvut_str.Length];
+
+            for (int i = 0; i < luvut_str.Length; i++) {
+                // Console.WriteLine(luvut_str[i]);
+                rivi[i] = int.Parse(luvut_str[i]);
+            }
+
+            bool isValid = TarkistaRivi(rivi);
+            if (isValid) {
+                Console.WriteLine("Validi rivi");
+            } else {
+                Console.WriteLine("huono rivi");
+            }
+        }
+
+        static void TulostaRivi(int[] rivi) {
+            for (int j = 0; j < rivi.Length; j++) {
+                Console.Write(rivi[j] + " "); 
+            } 
+            Console.WriteLine();
+        }
+
+        static int[] LuoRivi() {
+            int[] rivi = new int[7];
+
+            int lkm = 0;
+
+            Random r = new Random();
+            while (lkm < 7) {
+                int luku = r.Next(1, 40);
+
+                if (!rivi.Contains(luku)) {
+                    rivi[lkm] = luku;
+                    lkm++;
+                } 
+            }
+
+            return rivi;
+        }
+
+        static bool TarkistaRivi(int[] rivi) {
+            // pituus tasan 7
+            if (rivi.Length != 7) {
+                return false;
+            }
+            // luvut 1-40
+            foreach (var luku in rivi)
+            {
+                if (luku < 1 || luku > 40) {
+                    return false;
+                }
+            }
+            // uniikit luvut
+            Array.Sort(rivi);
+            for (int i = 0; i < rivi.Length - 1; i++) {
+                if (rivi[i] == rivi[i + 1]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
